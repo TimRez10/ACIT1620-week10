8cae2af40021fe2d85315a04807b6321fc81d80f
Timur R <timurreziapov2@gmail.com>
2022-11-09 14:10:44 -0800

    finalparticie

--- scripts/script.js
+++ scripts/script.js
@@ -29,11 +29,38 @@
 //     console.log(item)
 // }
 
-const person = {
-    name:['Timur', 'Reziapov'],
-    age:32,
-    gender:'male',
-    hobbies :['music', 'skiing']
-}
+// const person = {
+//     name:['Timur', 'Reziapov'],
+//     age:32,
+//     gender:'male',
+//     hobbies :['music', 'skiing']
+// }
 
-console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}`)
+// console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}`)
+
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3];
+
+let score = 0;
+for (let stu of students) {
+    if (stu.GPA > 3 && stu.program === 'CIT') {
+        score = score + 1;
+    };
+};
+console.log(score);
