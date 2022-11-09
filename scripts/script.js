eacea124c4f4f0a05d75b77dfc6c41348b3d8988
Timur R <timurreziapov2@gmail.com>
2022-11-09 13:53:43 -0800

    more for practice

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,24 @@
 //     i = i + 1
 //     console.log(2**i);
 // }
-let input;
-do {
-    input = prompt("What is your name");
-} while(!isNaN(Number(input))); 
 
-// alert("Hello " + input);
+// let input;
+// do {
+//     input = prompt("What is your name");
+// } while(!isNaN(Number(input))); 
 
-alert(`Hello ${input}`) 
+// // alert("Hello " + input);
+
+// alert(`Hello ${input}`) 
+
+let fruits = ['Banana','Apple']
+fruits.push('Orange')
+fruits.unshift('Mango')
+console.log(fruits)
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+for ( let item of fruits) {
+    console.log(item)
+}
